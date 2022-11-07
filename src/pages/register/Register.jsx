6c41d33043fe597ce2d3c60222ef9f70e8890f55
Jaganathan Govindhan <100895340+jaganathan20ITL116@users.noreply.github.com>
2022-11-07 15:39:47 +0530

    last

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,7 +20,7 @@ export default function Register() {
         email,
         password,
       });
-      res.data && window.location.replace("/login");
+      res.data && window.location.replace("auth/login");
     } catch (err) {
       setError(true);
     }
